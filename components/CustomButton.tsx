8eb0f498eeeef40a1664d4a46bff9bbb1f73d963
whiHak <betse1111111111@gmail.com>
2024-02-10 12:35:35 +0300

    modified and carCard component

---
+++ components/CustomButton.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { CustomButtonProps } from '@/types';
+import Image from 'next/image';
+import React from 'react'
+
+const CustomButton = ({title, btnStyles, btnType, textStyles,rightIcon,  handleClick}: CustomButtonProps) => {
+  return ( 
+    <button
+      disabled={false}
+      type={btnType || "button"}
+      className={`custom-btn ${btnStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && <Image src={`${rightIcon}`} alt='butto icon' width={20} height={20} />}
+    </button>
+  )
+}
+
+export default CustomButton
