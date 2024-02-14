bdb00e87e53d0dd96d6e63ae788d78318c4b06e4
whiHak <betse1111111111@gmail.com>
2024-02-15 02:29:32 +0300

    modified search funcitonality

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { FilterProps, HomeProps } from "@/types";
 import { fetchCars } from "@/utils";
+import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import Image from "next/image";
 
-export default async function Home() {
-
-  const allCars = await fetchCars();
-  console.log(allCars);
-
+export default async function Home(params: FilterProps ) {
+  const{searchParams:{manufacturer, model, year, fuel, limit  }} = params
+  const allCars = await fetchCars({
+    manufacturer: manufacturer || "",
+    model: model || "",
+    year: year || 2022,
+    fuel: fuel || "",
+    limit: limit || 10
+  });
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   return (
     <main className="overflow-hidden ">
@@ -20,20 +26,20 @@ export default async function Home() {
         <div className="home__filters">
           <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter title="fuel"/>
-            <CustomFilter title="year"/>
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
           </div>
         </div>
 
         {!isDataEmpty ? (
           <section>
             <div className="home__cars-wrapper">
-              {allCars?.map((car)=>
-                <CarCard car={car}/>
-              )}
+              {allCars?.map((car, id) => (
+                <CarCard car={car} key={id}/>
+              ))}
             </div>
           </section>
-        ):(
+        ) : (
           <div className="home__error-container">
             <h2 className="text-black text-xl font-bold">Oops.. no result</h2>
             <p>{allCars?.messsage}</p>
