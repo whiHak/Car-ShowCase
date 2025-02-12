247873bb90015ed9835afd65635f52a5df18d693
whiHak <betse1111111111@gmail.com>
2025-02-12 10:30:44 +0300

    added additional pages

---
+++ app/(root)/about-us/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+const AboutUs = async () => {
+
+  return (
+    <>
+      <section className="relative top-16 bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:p-5">
+        <h3 className="wrapper h3-bold text-center sm:text-left">About Us</h3>
+      </section>
+    </>
+  );
+};
+
+export default AboutUs;
