cb800930800b66f74d27fec1ab8d64c96516cc52
whiHak <betse1111111111@gmail.com>
2025-02-27 12:06:41 +0300

    test -6

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -91,8 +91,7 @@ export async function POST(req: Request) {
         photo: image_url,
       };
 
-      const newUser = await createuser(user);
-      return NextResponse.json({ message: "OK", user: newUser });
+      return NextResponse.json({ message: "OK", user: user });
     }
 
     if (eventType === 'user.deleted') {
